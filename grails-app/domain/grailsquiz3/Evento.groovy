6ead99b97a2bc348f177b22d0a0bab591230ea2e
Manuel E. Ureña <manuel.ureh@hotmail.com>
2016-11-10 18:35:38 -0400

    Creates evento domain

--- grails-app/domain/grailsquiz3/Evento.groovy
+++ grails-app/domain/grailsquiz3/Evento.groovy
@@ -2,6 +2,15 @@ package grailsquiz3
 
 class Evento {
 
+    String nombre
+    Date fechaInicio
+    Date fechaFin
+
+    static hasMany = [usuarios: Usuario]
+
     static constraints = {
+        nombre blank: false
+        fechaInicio blank: false
+        fechaFin blank: false
     }
 }
